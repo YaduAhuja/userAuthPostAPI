19b7943307c17593119608a8046982f0e9dfd8e4
Yadunandan Ahuja <yaduahuja24@gmail.com>
2021-07-29 14:57:41 +0530

    Remove Extra Logs

--- src/middleware/requiresUser.ts
+++ src/middleware/requiresUser.ts
@@ -2,12 +2,12 @@ import { get } from "lodash";
 import { Request, Response, NextFunction } from "express";
 import log from "../logger";
 
-export default async function requiresUser (
+export default async function requiresUser(
 	req: Request,
 	res: Response,
 	next: NextFunction
 ) {
-	log.error(req);
+	// log.error(req);
 	const user = get(req, "user");
 	if (!user) {
 		return res.sendStatus(403);
